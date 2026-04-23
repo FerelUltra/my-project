a57311f367f1a4a721faf2ba219eb81f069065ec
Ferel <66280242+FerelUltra@users.noreply.github.com>
2026-04-24 00:48:12 +0300

    feat: add leetcode problem №24

---
+++ src/bin/24_swap_nodes_in_paris.rs
@@ -0,0 +1,31 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+fn main() {}
+
+pub fn swap_pairs(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+
+	let mut dummy = Box::new(ListNode{
+		val: 0,
+		next: head
+	});
+
+	let mut tail = &mut dummy;
+
+	while tail.next.is_some() && tail.next.as_ref().unwrap().next.is_some() {
+		let mut first = tail.next.take().unwrap();
+		let mut second = first.next.take().unwrap();
+		let rest = second.next.take();
+
+		first.next = rest;
+		second.next = Some(first);
+		tail.next = Some(second);
+
+		tail = tail.next.as_mut().unwrap().next.as_mut().unwrap();
+	}
+
+	dummy.next
+}
